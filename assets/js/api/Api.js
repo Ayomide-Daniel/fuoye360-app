93eccc040a535f17a860847a0b9c1a6d78ce789d
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-04-19 00:42:25 +0100

    API.js patch

--- assets/js/api/Api.js
+++ assets/js/api/Api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
+import $ from 'jquery'
 const Api = axios.create({
   baseURL: 'https://fuoye360.com/api',
 })
 Api.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 Api.defaults.withCredentials = true
+Api.get('/csrf-cookie').then(() => {
+  Api.defaults.headers.common['X-CSRF-TOKEN'] = $(
+    'meta[name="csrf-token"]'
+  ).attr('content')
+})
 export default Api
